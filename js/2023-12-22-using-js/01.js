b6dc9386ad368f4815c1234cd981f1741955e0a6
veyselkayhan <veyselkayhan13@gmail.com>
2023-12-22 13:56:46 +0300

    first commit

---
+++ js/2023-12-22-using-js/01.js
@@ -0,0 +1,18 @@
+// single line code comment (tek satır yorum)
+
+/*
+multi
+line
+code
+comment
+*/
+
+/*
+
+output yöntemlerinde iki tanesi 
+alert();
+console.log();
+
+*/
+
+alert(" Hello Word from the external js file");
